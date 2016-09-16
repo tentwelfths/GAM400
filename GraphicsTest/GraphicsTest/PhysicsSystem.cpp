24e39e70b4e94d6b66a66503c30eb3946fa37505
Weylin <w.etra@digipen.edu>
2016-09-16 12:21:02 -0700

    collider detection should be in

--- GraphicsTest/GraphicsTest/PhysicsSystem.cpp
+++ GraphicsTest/GraphicsTest/PhysicsSystem.cpp
@@ -35,10 +35,11 @@ void PhysicsSystem::Update(double dt)
 
       auto firstPos = firstTrans->mPosition() + firstBox->GetOffset();
       auto secondPos = secondTrans->mPosition() + secondBox->GetOffset();
-      if ((firstPos.x < (secondPos.x + secondBox->GetHalfSize().x)) &&
-        ((firstPos.x + firstBox->GetHalfSize().x) > secondPos.x) &&
-        (firstPos.y < (secondPos.y + secondBox->GetHalfSize().y)) && 
-        ((firstPos.y + firstBox->GetHalfSize().y) > secondPos.y))
+      
+      if (((firstPos.x - firstBox->GetHalfSize().x) < (secondPos.x + secondBox->GetHalfSize().x)) &&
+        ((firstPos.x + firstBox->GetHalfSize().x) > (secondPos.x - secondBox->GetHalfSize().x)) &&
+        ((firstPos.y - firstBox->GetHalfSize().y) < (secondPos.y + secondBox->GetHalfSize().y)) &&
+        ((firstPos.y + firstBox->GetHalfSize().y) > (secondPos.y - secondBox->GetHalfSize().y)))
       {
         Collision col;
         col.AddObjects(firstTrans->mParent(), secondTrans->mParent());
@@ -47,7 +48,39 @@ void PhysicsSystem::Update(double dt)
     }
     for (auto & iter2 : mSphereColliders_)
     {
-      
+      auto firstTrans = iter->mParent()->GetComponentA<TransformComponent>("TransformComponent");
+      auto secondTrans = iter2->mParent()->GetComponentA<TransformComponent>("TransformComponent");
+      auto firstBox = iter->mParent()->GetComponentA<BoxColliderComponent>("BoxColliderComponent");
+      auto secondSphere = iter2->mParent()->GetComponentA<SphereColliderComponent>("SphereColliderComponent");
+
+      auto firstPos = firstTrans->mPosition() + firstBox->GetOffset();
+      auto secondPos = secondTrans->mPosition() + secondSphere->GetOffset();
+
+      vec3 copyPos = secondPos;
+
+      if (copyPos.x > firstPos.x + firstBox->GetHalfSize().x)
+      {
+        copyPos.x = firstPos.x + firstBox->GetHalfSize().x;
+      }
+      else if (copyPos.x < firstPos.x - firstBox->GetHalfSize().x)
+      {
+        copyPos.x = firstPos.x - firstBox->GetHalfSize().x;
+      }
+      if (copyPos.y > firstPos.y + firstBox->GetHalfSize().y)
+      {
+        copyPos.y = firstPos.y + firstBox->GetHalfSize().y;
+      }
+      else if (copyPos.y < firstPos.y - firstBox->GetHalfSize().y)
+      {
+        copyPos.y = firstPos.y - firstBox->GetHalfSize().y;
+      }
+      float dist = ((copyPos.x + secondPos.x) * (copyPos.x + secondPos.x)) + ((copyPos.y + secondPos.y) * (copyPos.y + secondPos.y));
+      if (dist < secondSphere->GetRadius())
+      {
+        Collision col;
+        col.AddObjects(firstTrans->mParent(), secondTrans->mParent());
+        mCollision_.push_back(col);
+      }
     }
   }
   for (auto & iter : mSphereColliders_)
@@ -55,7 +88,25 @@ void PhysicsSystem::Update(double dt)
     iter->Update(dt);
     for (auto iter2 = iter; iter2 != mSphereColliders_.back(); ++iter2)
     {
+      float r = iter->GetRadius() + iter2->GetRadius();
+      r *= r;
+      auto firstTrans = iter->mParent()->GetComponentA<TransformComponent>("TransformComponent");
+      auto secondTrans = iter2->mParent()->GetComponentA<TransformComponent>("TransformComponent");
+      auto firstSphere = iter->mParent()->GetComponentA<SphereColliderComponent>("SphereColliderComponent");
+      auto secondSphere = iter2->mParent()->GetComponentA<SphereColliderComponent>("SphereColliderComponent");
 
+      auto firstPos = firstTrans->mPosition() + firstSphere->GetOffset();
+      auto secondPos = secondTrans->mPosition() + secondSphere->GetOffset();
+
+      float x = (firstPos.x + secondPos.x) * (firstPos.x + secondPos.x);
+      float y = (firstPos.y + secondPos.y) * (firstPos.y + secondPos.y);
+
+      if (r < (x + y))
+      {
+        Collision col;
+        col.AddObjects(firstTrans->mParent(), secondTrans->mParent());
+        mCollision_.push_back(col);
+      }
     }
   }
   for (auto & iter : mCollision_)
@@ -77,18 +128,15 @@ void PhysicsSystem::RegisterComponent(PhysicsComponent * comp)
 
 void PhysicsSystem::RegisterComponent(RigidbodyComponent * comp)
 {
-  mComponents_.push_back(comp);
   mRigidbodies_.push_back(comp);
 }
 
 void PhysicsSystem::RegisterComponent(BoxColliderComponent * comp)
 {
-  mComponents_.push_back(comp);
   mBoxColliders_.push_back(comp);
 }
 
 void PhysicsSystem::RegisterComponent(SphereColliderComponent * comp)
 {
-  mComponents_.push_back(comp);
   mSphereColliders_.push_back(comp);
 }
