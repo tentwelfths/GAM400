421e8bde1ed57aed6de1f452e0dd89940c851c86
Joel <tentwelfths@gmail.com>
2016-10-11 14:03:33 -0700

    moved garbage

--- GraphicsTest/RaspApp/GraphicsSystem.cpp
+++ GraphicsTest/RaspApp/GraphicsSystem.cpp
@@ -1,5 +1,23 @@
 #include "GraphicsSystem.h"
 #include "Object.h"
+inline std::string& rtrim(std::string& s, const char* t = " \t\n\r\f\v")
+{
+  s.erase(s.find_last_not_of(t) + 1);
+  return s;
+}
+
+// trim from beginning of string (left)
+inline std::string& ltrim(std::string& s, const char* t = " \t\n\r\f\v")
+{
+  s.erase(0, s.find_first_not_of(t));
+  return s;
+}
+
+// trim from both ends of string (left & right)
+inline std::string& trim(std::string& s, const char* t = " \t\n\r\f\v")
+{
+  return ltrim(rtrim(s, t), t);
+}
 
 glm::mat4 setUpRotationMatrix(glm::mat4 rotationMatrix, float angle, float u, float v, float w);
 
