fd87929f5b1e74b895f34732d99d51d705f89ada
Joel <tentwelfths@gmail.com>
2016-10-11 14:02:39 -0700

    some garbage

--- GraphicsTest/RaspApp/Object.h
+++ GraphicsTest/RaspApp/Object.h
@@ -14,3 +14,22 @@ class Object
 };
 
 extern std::unordered_map<unsigned int, Object*> gObjects[50];
+
+inline std::string& rtrim(std::string& s, const char* t = " \t\n\r\f\v")
+{
+  s.erase(s.find_last_not_of(t) + 1);
+  return s;
+}
+
+// trim from beginning of string (left)
+inline std::string& ltrim(std::string& s, const char* t = " \t\n\r\f\v")
+{
+  s.erase(0, s.find_first_not_of(t));
+  return s;
+}
+
+// trim from both ends of string (left & right)
+inline std::string& trim(std::string& s, const char* t = " \t\n\r\f\v")
+{
+  return ltrim(rtrim(s, t), t);
+}
