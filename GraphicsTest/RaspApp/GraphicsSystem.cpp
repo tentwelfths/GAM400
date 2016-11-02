2e198edd68a729a042457d67702591970dc736e2
Joel <tentwelfths@gmail.com>
2016-11-01 22:12:54 -0700

    clearcolor

--- GraphicsTest/RaspApp/GraphicsSystem.cpp
+++ GraphicsTest/RaspApp/GraphicsSystem.cpp
@@ -188,7 +188,8 @@ GraphicsSystem::GraphicsSystem()
   glEnable(GL_DEPTH_TEST);
   glEnable(GL_BLEND);
   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-  glClearColor(0.0f, 0.4f, 0.03f, 1.0f);
+  //glClearColor(0.0f, 0.4f, 0.03f, 1.0f);
+  glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
   
   
   GLfloat vVertices[] = { -0.5f,  0.5f, 0.0f,  // Position 0
