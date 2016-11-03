6003bc413dc3e29349f15ce1219ddf1614c32189
Joel <tentwelfths@gmail.com>
2016-11-03 12:44:05 -0700

    CONNNE

--- GraphicsTest/GraphicsTest/ConeComponent.cpp
+++ GraphicsTest/GraphicsTest/ConeComponent.cpp
@@ -28,8 +28,8 @@ void ConeComponent::Update(double dt)
   auto * trans = mParent()->GetComponent(TransformComponent);
   auto * transPlayer = thePlayer->GetComponent(TransformComponent);
   auto * theCone = thePlayer->GetComponent(ConeControllerComponent);
-  float x = transPlayer->mPositionX() + theCone->GetX(); 
-  float y = transPlayer->mPositionY() + trans->mScaleY() / 2.f + theCone->GetY();
+  float x = transPlayer->mPositionX() + theCone->GetX() * (trans->mScaleX() / 2.f); 
+  float y = transPlayer->mPositionY() + theCone->GetY() * (trans->mScaleY() / 2.f);
   bool xCheck = trans->mPositionX() > x - EPIFORTRANS && trans->mPositionX() < x + EPIFORTRANS;
   bool yCheck = trans->mPositionY() > y - EPIFORTRANS && trans->mPositionY() < y + EPIFORTRANS;
 
