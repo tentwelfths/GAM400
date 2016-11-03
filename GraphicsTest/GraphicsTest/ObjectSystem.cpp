815cc3d4861c896e278f0790eeb60a536427e339
Weylin <w.etra@digipen.edu>
2016-11-02 19:12:49 -0700

    should fix deletion

--- GraphicsTest/GraphicsTest/ObjectSystem.cpp
+++ GraphicsTest/GraphicsTest/ObjectSystem.cpp
@@ -261,6 +261,7 @@ void     ObjectSystem::Update(double dt)
           mObjectMap_.erase(mObjectMap_.find(node->value->ID));
           delete node->value;
           node = iter->second.Remove(node);
+          node = nullptr;
         }
         else{
           node->value->Update(dt);
