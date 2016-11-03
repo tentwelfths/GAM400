44ce1b77607966a400ff8f88b27bf42690a84957
Joel <tentwelfths@gmail.com>
2016-11-03 13:14:12 -0700

    WHGAT?

--- GraphicsTest/RaspApp/GraphicsSystem.cpp
+++ GraphicsTest/RaspApp/GraphicsSystem.cpp
@@ -320,7 +320,7 @@ void GraphicsSystem::Draw()
   
     // Clear the color buffer
   glColorMask(false, false, false, true);
-  glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+  glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   glColorMask(true, true, true, true);
   if(viewChanged){
