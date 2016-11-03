a86def1814c1489c35820e5cb547e1f4010b52b8
Weylin <w.etra@digipen.edu>
2016-11-02 19:16:32 -0700

    added nullptr check

--- GraphicsTest/GraphicsTest/BulletComponent.cpp
+++ GraphicsTest/GraphicsTest/BulletComponent.cpp
@@ -28,14 +28,14 @@ void BulletComponent::Update(double dt)
       CollisionStartedMessage * col = reinterpret_cast<CollisionStartedMessage *>(iter.data);
       if (col->obj1 == mParent())
       {
-        if (col->obj2->name != "Knife" && col->obj2->name != "Coneman" && col->obj2->name != "Cone")
+        if (col->obj2 != nullptr && (col->obj2->name != "Knife" && col->obj2->name != "Coneman" && col->obj2->name != "Cone"))
         {
           contact = true;
         }
       }
       else
       {
-        if (col->obj1->name != "Knife" && col->obj1->name != "Coneman" && col->obj1->name != "Cone")
+        if (col->obj1 != nullptr && (col->obj1->name != "Knife" && col->obj1->name != "Coneman" && col->obj1->name != "Cone"))
         {
           contact = true;
         }
