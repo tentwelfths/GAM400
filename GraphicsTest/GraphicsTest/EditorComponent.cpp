6c3052023ac649abaf5ffd3247d9780337dfb076
Joel <tentwelfths@gmail.com>
2016-11-11 14:09:46 -0800

    Editor changes

--- GraphicsTest/GraphicsTest/EditorComponent.cpp
+++ GraphicsTest/GraphicsTest/EditorComponent.cpp
@@ -6,7 +6,7 @@
 #include "TransformComponent.h"
 #include "Object.h"
 
-EditorComponent::EditorComponent(){
+EditorComponent::EditorComponent() : tile(false){
   EditorSystem * g = gCore->GetSystem(EditorSystem);
   g->RegisterComponent(this);
 
@@ -22,25 +22,21 @@ void EditorComponent::Update(double dt){
   auto * trans = mParent_->GetComponent(TransformComponent);
   double mX = input->GetMouseX();
   double mY = input->GetMouseY();
-  if ((input->isKeyPressed(GLFW_MOUSE_BUTTON_1) && editor->tileEditorActive) || (input->isKeyJustPressed(GLFW_MOUSE_BUTTON_1) && !editor->tileEditorActive)){
+  if ((tile ^ editor->tileEditorActive)){
+    return;
+  }
+  if ((input->isKeyPressed(GLFW_MOUSE_BUTTON_1) && editor->tileEditorActive) || (input->isKeyJustPressed(GLFW_MOUSE_BUTTON_1) && !editor->tileEditorActive && !tile)){
     
     if (mX >= trans->mPositionX() - trans->mScaleX() / 2.f && mX <= trans->mPositionX() + trans->mScaleX() / 2.f
       && mY >= trans->mPositionY() - trans->mScaleY() / 2.f && mY <= trans->mPositionY() + trans->mScaleY() / 2.f){
-      //std::cout << mX << "," << mY << std::endl;
-      //std::cout << trans->mPositionX() - trans->mScaleX() / 2.f << "," << trans->mPositionX() + trans->mScaleX() / 2.f << std::endl;
-      //std::cout << trans->mPositionY() - trans->mScaleY() / 2.f << "," << trans->mPositionY() + trans->mScaleY() / 2.f << std::endl << std::endl;
-      auto * editor = gCore->GetSystem(EditorSystem);
       editor->Select(mParent_);
     }
   }
-  else if (input->isKeyPressed(GLFW_MOUSE_BUTTON_2)){
+  else if ((input->isKeyPressed(GLFW_MOUSE_BUTTON_2) && editor->tileEditorActive) || (input->isKeyJustPressed(GLFW_MOUSE_BUTTON_2) && !editor->tileEditorActive && !tile)){
     
     if (mX >= trans->mPositionX() - trans->mScaleX() / 2.f && mX <= trans->mPositionX() + trans->mScaleX() / 2.f
       && mY >= trans->mPositionY() - trans->mScaleY() / 2.f && mY <= trans->mPositionY() + trans->mScaleY() / 2.f){
-      //std::cout << mX << "," << mY << std::endl;
-      //std::cout << trans->mPositionX() << "," << trans->mPositionY() << std::endl << std::endl;
       mParent_->Destroy();
-      auto * editor = gCore->GetSystem(EditorSystem);
       editor->Select(nullptr);
     }
   }
