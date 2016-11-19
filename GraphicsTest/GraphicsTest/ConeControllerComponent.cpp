494c2605b9aa61ce440ce1f88df176c0a2a34f8f
Weylin <w.etra@digipen.edu>
2016-11-19 13:13:40 -0800

    tuning so fine

--- GraphicsTest/GraphicsTest/ConeControllerComponent.cpp
+++ GraphicsTest/GraphicsTest/ConeControllerComponent.cpp
@@ -159,7 +159,7 @@ void ConeControllerComponent::UpdateVis(Object* theTarget)
   }
   theDistance.Normalize();
   float theCosine = mDirX * theDistance.x + mDirY * theDistance.y;
-  if (-theCosine > 0.85f)
+  if (-theCosine > 0.87f)
   {
     for (int i = 0; i < 4; ++i)
     {
