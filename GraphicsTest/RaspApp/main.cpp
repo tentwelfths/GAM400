9505db53d0601962b4d2e14fb3aeaeb11eb5b67e
Joel <tentwelfths@gmail.com>
2016-12-10 14:47:00 -0800

    ethjwrejn

--- GraphicsTest/RaspApp/main.cpp
+++ GraphicsTest/RaspApp/main.cpp
@@ -557,14 +557,14 @@ int main ( int argc, char *argv[] )
   GunController gun;
 
   if(strcmp(argv[1], "cone")==0){
-    myID = 0;
+    myID = 01;
     gun.Initialize();
     controller = &gun;
     //cone.Initialize();
     //controller = &cone;
   }
   else if(strcmp(argv[1], "gun")==0){
-    myID = 01;
+    myID = 0;
     gun.Initialize();
     controller = &gun;
   }
