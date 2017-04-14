e350f67b33c6a08a8a6a1a95b70140ebaebaf1cf
Joel <tentwelfths@gmail.com>
2017-04-14 11:05:42 -0700

    No more seeding and all the files

--- GraphicsTest/GraphicsTest/Core.cpp
+++ GraphicsTest/GraphicsTest/Core.cpp
@@ -19,7 +19,7 @@ void Core::RegisterSystem(System * s)
 
 bool Core::Initialize()
 {
-  srand(time(NULL));
+  //srand(time(NULL));
   editor = false;
   active = true;
   auto * g = new GraphicsSystem();
